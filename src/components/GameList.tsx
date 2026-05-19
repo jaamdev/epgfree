56d4c4925b617deaa7c15d670a69259ae12cfbe3
JaamDev <jaamdev@hotmail.com>
2026-05-19 11:41:34 +0200

    style(GameList): Improve the UI

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -18,9 +18,8 @@ export default function GameList({ list }: Props) {
 
   return <ul
     className='
-      size-full m-auto px-2 py-1 max-w-6xl
-      grid grid-cols-1 md:grid-cols-[repeat(auto-fit,minmax(200px,1fr))]
-      justify-center items-center gap-2
+      size-full m-auto p-2 max-w-6xl
+      flex flex-col sm:flex-row flex-nowrap justify-center gap-2
     '
   >
     {list.map(({ titleGame, urlGame, urlImg, currentFree }) => (
@@ -29,16 +28,12 @@ export default function GameList({ list }: Props) {
         href={urlGame}
         rel='noopener noreferrer nofollow external'
         target='_blank'
-        className='
-          size-full m-auto group hover:scale-102 rounded-md overflow-hidden
-          transition duration-300
-          flex flex-col flex-nowrap justify-center items-center
-        '
+        className='size-max group hover:scale-102 rounded-md overflow-hidden transition duration-300'
       >
-        <h3 className={`w-full px-1 py-2 m-auto text-center uppercase tracking-wide font-semibold ${currentFree ? 'bg-primary' : 'bg-black'}`}>{currentFree ? 'gratis ahora' : 'próximamente'}</h3>
-        <img alt={titleGame} src={urlImg} className='size-full object-cover' />
+        <h3 className={`w-full p-2 text-center uppercase tracking-wide font-semibold ${currentFree ? 'bg-primary' : 'bg-black'}`}>{currentFree ? 'gratis ahora' : 'próximamente'}</h3>
+        <img alt={titleGame} src={urlImg} className='object-fill' />
         <h2 className='
-            w-full px-1 py-2 text-center bg-secondary
+            w-full p-2 text-center bg-secondary
             group-hover:bg-primary text-xl font-semibold
             transition duration-300
           '>{titleGame}</h2>
